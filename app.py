fcbe07aaccf29fd50e94115f5a2069abe97827ea
Snasset <145303291+Snasset@users.noreply.github.com>
2025-06-19 06:15:31 +0000

    ytpo

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ from util_helper.preproc import resize_img
 import subprocess
 
 from paddleocr import PaddleOCR
-st.write(f"Versi paddleocr (wrapper): **{paddleocr.__version__}**")
+st.write(f"Versi paddleocr (wrapper): **{PaddleOCR.__version__}**")
 # import pytesseract
 ocr = PaddleOCR(text_detection_model_dir='./paddleppocr/en_PP-OCRv3_det_infer/inference',
                 # text_recognition_model_dir='./paddleppocr/best_model_50k/paddlev3_50k/inference',
