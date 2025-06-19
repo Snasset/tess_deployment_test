46a1aaf521e0d8cbef54b87785565341a5819f8d
Snasset <145303291+Snasset@users.noreply.github.com>
2025-06-19 03:06:25 +0000

    change dir

--- app.py
+++ app.py
@@ -12,8 +12,8 @@ import subprocess
 
 from paddleocr import PaddleOCR
 # import pytesseract
-ocr = PaddleOCR(text_recognition_model_dir='./paddleppocr/best_model_50k/paddlev3_50k/inference',
-                text_detection_model_dir='./paddleppocr/en_PP-OCRv3_det_infer/inference',
+ocr = PaddleOCR(text_detection_model_dir='./paddleppocr/en_PP-OCRv3_det_infer/inference',
+                text_recognition_model_dir='./paddleppocr/best_model_50k/paddlev3_50k/inference',
                 # rec_char_dict_path='./paddleppocr/en_dict.txt',
                 textline_orientation_model_dir='./paddleppocr/ch_ppocr_mobile_v2.0_cls_infer',
                 use_textline_orientation=False)
